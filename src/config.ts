776cc2f25bba654be934e7618709e3940ba8c57c
早弦 <tianyi.mty@antfin.com>
2019-09-15 00:45:03 +0800

    初步完成生成网页截图

---
+++ src/config.ts
@@ -0,0 +1,4 @@
+export default {
+  devServerHost: 'http://localhost', // 开发服务器 Host 地址
+  distDir: '.urtimages' // 生成图片的存放目录
+};
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,43 @@
-// ref:
-// - https://umijs.org/plugin/develop.html
+import puppeteer from 'puppeteer';
+import 'regenerator-runtime/runtime'; // temp
+import crypto from 'crypto';
+import fs from 'fs';
+import config from './config';
 
-export default function (api, options) {
+let count = 1;
 
-  // Example: output the webpack config
-  api.chainWebpackConfig(config => {
-    console.log(config.toString());
+const screenshot = async (route, devServerUrl) => {
+  const { pagePath, imagePath } = await genPath(route, devServerUrl);
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(pagePath);
+  await page.screenshot({ path: imagePath });
+
+  await browser.close();
+};
+
+const genPath = async (route, devServerUrl) => {
+  const { path } = route;
+  const pagePath = `${devServerUrl}${path}`;
+  const md5Path = crypto
+    .createHash('md5')
+    .update(path)
+    .digest('hex');
+  const folderDir = `${config.distDir}/${count}`;
+  await fs.mkdir(folderDir, { recursive: true }, err => {
+    if (err) throw err;
+  });
+  const imagePath = `${folderDir}/${md5Path}.png`;
+  return {
+    pagePath,
+    imagePath
+  };
+};
+
+export default function(api, options) {
+  api.afterDevServer(({ serve, devServerPort }) => {
+    const devServerUrl = `${config.devServerHost}:${devServerPort}`;
+    screenshot(api.routes[0], devServerUrl);
   });
 
   api.addUIPlugin(require.resolve('../dist/index.umd'));
@@ -13,9 +45,8 @@ export default function (api, options) {
   api.onUISocket(({ action, failure, success }) => {
     if (action.type === 'org.Tianyi Ma.umi-regression-test.test') {
       success({
-        data: 'umi-regression-test.test',
+        data: 'umi-regression-test.test'
       });
     }
   });
-
 }
